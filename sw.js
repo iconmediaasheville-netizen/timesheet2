f3865c2152c5ff42ae9e6d6f5282f6a6c6b6383d
iconmediaasheville-netizen <iconmediaasheville@gmail.com>
2026-08-15 10:58:32 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,11 @@
-const CACHE_NAME = 'timesheet-v1';
-const ASSETS = [
-  './',
-  './index.html'
-];
-
-// Install Service Worker and Cache App Files
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
   self.skipWaiting();
 });
 
-// Serve Cached Files When Offline
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(e.request).catch(() => {
-        return caches.match('./index.html');
-      });
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
